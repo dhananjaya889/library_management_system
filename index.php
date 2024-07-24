012801aab2b49b19d7d1f7093cea2070a74ebf02
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2024-07-24 12:28:45 +0530

    Add home page

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Library Management System</title>
+    </head>
+
+    <body>
+        <nav>
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">about</a></li>
+                    <li><a href="book.php">Find Books</a></li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                    <li><a href="login.php">Log In</a></li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Library Management System</title>
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            
+        </div>
+    </nav>
+</body>
+</html>
