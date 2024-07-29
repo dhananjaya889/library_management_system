28278ab987a92ca88606fd87e15214c6932bc8ec
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2024-07-29 19:10:07 +0530

    add about page

---
+++ frontend/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us Page</title>
+</head>
+<body>
+    <div class="about">
+        <h1>About Us Page</h1>
+    </div>
+    <h2>Our Team</h2>
+    <div class=""></div>
+</body>
+</html>
