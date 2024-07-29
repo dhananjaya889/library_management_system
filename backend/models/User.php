8d3171e460f254aeaaa8c194a21b6dff418e8334
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2024-07-30 01:00:32 +0530

    create models

---
+++ backend/models/User.php
@@ -0,0 +1,34 @@
+<?php
+
+    require_once __DIR__."/../config/Repository.php";
+
+    class User
+    {
+        protected $id;
+        protected $name;
+        protected $email;
+        protected $password;
+
+        protected $tableName;
+
+        protected $colomns;
+
+        protected $repo;
+
+        public function __construct(){
+            $this->repo = new Repository();
+        }
+
+        public function getAll(){
+            return $this->repo->all($this->tableName);
+        }
+
+        public function create($request){
+            $data = [
+                'colomns'=> $this->colomns,
+                'values' => $request
+            ];
+            return $this->repo->insert($this->tableName, $data);
+        }
+    }
+?>
--- frontend/signup.php
+++ frontend/signup.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
+    require_once __DIR__."../backend/models/User.php";
+
+    $user = new User();
+
+    $result = $user->getAll();
+    
+?>
+
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
     <link rel="stylesheet" href="style.css">
 </head>
