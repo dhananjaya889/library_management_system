2043b41457c52178aa486f2c79850b58cca8e40c
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2024-07-29 10:42:39 +0530

    add header

---
+++ frontend/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Library Management System</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="image">
+            
+            <?php
+            include("header.html");
+            ?>
+
+        <div>
+            <section>
+                <h1>The Library Management System</h1>
+                <p>Manage the Library</p>
+            </section>
+        </div>
+    </div>
+    </body>
+</html>
