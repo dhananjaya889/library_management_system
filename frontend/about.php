822e6057e3116239150691c86e21981b61c3d878
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2024-08-02 23:35:06 +0530

    about us

--- frontend/about.php
+++ frontend/about.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us Page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <div class="about">
-        <h1>About Us Page</h1>
+    <div>
+        <div>
+            <h1>About Us</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            </p>
+            <div class="social-icons-container">
+                <a class="social-icon">
+                    <img class="image-block" src="img/fb.png">
+                </a>
+                <a class="social-icon">
+                    <img class="image-block" src="img/in.png">
+                </a>
+                <a class="social-icon">
+                    <img class="image-block" src="img/inst.png">
+                </a>
+                <a class="social-icon">
+                    <img class="image-block" src="img/tiw.png">
+                </a>
+            </div>
+        </div>
     </div>
-    <h2>Our Team</h2>
-    <div class=""></div>
 </body>
+
 </html>
